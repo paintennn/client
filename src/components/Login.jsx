251c9caf510e3bf97d6c52174b6f58495351eef3
paintennn <thinhphan1171@gmail.com>
2024-09-30 14:03:53 +0700

    11

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { app } from '../config/firebase.config'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { FcGoogle } from 'react-icons/fc'
+
 const Login = () => {
+
+    const firebaseAuth = getAuth(app)
+    const provider = new GoogleAuthProvider()
+
+    // const navigate = useNavigate()
+
+    const loginWithGoogle = async () => {
+        await signInWithPopup(firebaseAuth, provider).then((userCred) => {
+            console.log(userCred)
+        })
+    }
+
     return (
         <div className="relative w-screen h-screen">
             <div className="absolute inset-0 bg-slate-400 flex items-center justify-center p-4">
                 <div className="w-full md:w-48 p-4 bg-slate-200 shadow-2x1 rounded-md backdrop-blur-md flex-col items-center justify-center">
-                    <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-slate-300 cursor-pointer hover:bg-slate-400 hover:shadow-md duration-100 ease-in-out transition-all">
-                        <FcGoogle />
+                    <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-slate-300 cursor-pointer hover:bg-slate-400 hover:shadow-md duration-100 ease-in-out transition-all" onClick={loginWithGoogle}>
+                        <FcGoogle className="text-xl" />
                         Sign in with Google
                     </div>
+
+
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client"; // Use 'react-dom/client' for React 18
+import "./index.css";
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import "./index.css"
-import App from './App'
+// Get the root element
+const rootElement = document.getElementById('root');
 
-import { BrowserRouter as Router } from 'react-router-dom'
+// Create a root and render the app
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
 
-ReactDom.render(
-    <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-
-    </React.StrictMode>
-    , document.getElementById('root'))
 
 
