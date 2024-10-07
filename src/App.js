0df864fc48435f7609b40a3eb514725fdb54c2fd
Thinh <thinhphan1171@gmail.com>
2024-10-07 11:55:06 +0700

    adding verify google

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-import {Route, Routes} from 'react-router-dom'
-import './index.css'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { Home, Login } from "./components"
+import { app } from "./config/firebase.config"
+import { getAuth } from "firebase/auth"
+
 const App = () => {
+
+    const firebaseAuth = getAuth(app)
+    const [authState, setAuthState] = useState(false)
+
+    const navigate = useNavigate()
+    const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true")
+
+    useEffect(() => {
+        firebaseAuth.onAuthStateChanged((userCred) => {
+            if (userCred) {
+                userCred.getIdToken().then((token) => {
+                    console.log(token)
+                })
+            }else{
+                setAuth(false)
+                window.localStorage.setItem("auth","false")
+                navigate("/login")
+            }
+        })
+    }, [])
     return (
         <div className="w-screen h-screen bg-blue-400 flex justify-center items-center">
             <Routes>
-                <Route path='Login' element={<Login/>}/>
-                <Route path='/*' element={<Home/>}/>
+                <Route path='Login' element={<Login setAuth={setAuth}/>} />
+                <Route path='/*' element={<Home />} />
             </Routes>
         </div>
     )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import { app } from '../config/firebase.config'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { FcGoogle } from 'react-icons/fc'
+import { replace, useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ setAuth }) => {
 
     const firebaseAuth = getAuth(app)
     const provider = new GoogleAuthProvider()
 
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
 
     const loginWithGoogle = async () => {
         await signInWithPopup(firebaseAuth, provider).then((userCred) => {
-            console.log(userCred)
+            if (userCred) {
+                setAuth(true)
+                window.localStorage.setItem("auth", "true")
+                firebaseAuth.onAuthStateChanged((userCred) => {
+                    if(userCred){
+                        console.log(userCred)
+                        navigate("/",{replace:true})
+                    }
+                    else{
+                        setAuth(false)
+                        navigate("/login")
+                    }
+                })
+            }
         })
     }
 
